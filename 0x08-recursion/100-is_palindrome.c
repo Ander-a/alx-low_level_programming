062c3b9a4e2187d6cc8123a07589eebdf4b30b7b
Andera Innocent <anderainnocent@gmail.com>
2023-10-25 14:41:50 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,13 +11,25 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
+	if (*s == 0)
+		return (1);
+	return (check_pals(s, 0, _strlen_recursion(s)));
+}
+/**
+ * _strlen_recursion-returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
 	if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * check_pal-checks the character recursively for palindrome
+ * check_pal-checks the characters recursively for palindrome
  * @s: string to check
  * @i: iterator
  * @len: length of the string
